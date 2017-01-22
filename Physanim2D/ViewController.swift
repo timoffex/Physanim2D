be153a69309ad2098ed3c5b16885dee54e382566
timoffex <timoffex@gmail.com>
2017-01-21 16:19:38 -0800

    Initial Commit

---
+++ Physanim2D/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Physanim2D
+//
+//  Created by Tima Peshin on 1/21/17.
+//  Copyright © 2017 timoffex. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
